75c1bff5b6c8dc5b08e27b7dfeff668dcc23e925
paoloevan <paoloev1900@gmail.com>
2022-11-30 15:15:46 +0100

    add json file

--- index.php
+++ index.php
@@ -19,6 +19,10 @@
             <ul class="list-group">
                 <li class="list-group-item" v-for="task in todoList">{{task}}</li>
             </ul>
+            <form action="server.php" method="POST">
+                <input class="form-control my-2" type="text" name="new_task" id="new_task" placeholder="Insert new task">
+                <button class="btn btn-primary" type="submit">Submit</button>
+            </form>
         </div>
     </div>
 
--- server.php
+++ server.php
@@ -1,13 +1,17 @@
 <?php
 
-$todo = [
-    'HTML',
-    'CSS',
-    'Responsive design',
-    'Javascript',
-    'PHP',
-    'Laravel'
-];
+// $todo = [
+//     'HTML',
+//     'CSS',
+//     'Responsive design',
+//     'Javascript',
+//     'PHP',
+//     'Laravel'
+// ];
+
+$todo = file_get_contents('tasks.json');
+$todo_array = json_decode($todo);
+
 
 header('Content-Type: application/json');
-echo json_encode($todo);
+echo json_encode($todo_array);

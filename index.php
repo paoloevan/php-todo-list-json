e2a9aa3b5c90b60801408ec81f844fd9c30fddf8
paoloevan <paoloev1900@gmail.com>
2022-12-01 16:06:49 +0100

    fix bug

--- index.php
+++ index.php
@@ -9,22 +9,23 @@
     <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi' crossorigin='anonymous'>
 </head>
 
-<body id='app'>
+<body>
+    <div id='app'>
 
-    <h1 class="text-center my-3">Todo List</h1>
+        <h1 class="text-center my-3">Todo List</h1>
 
-    <div class="container d-flex justify-content-center">
-        <div class="col-4">
-            <ul class="list-group">
-                <li class="list-group-item" v-for="task in todoList">{{task.text}}</li>
-            </ul>
-            <div>
-                <input @keyup.enter="saveTasks()" v-model='task' class="form-control my-2" type="text" name="new_task" id="new_task" placeholder="Insert new task">
-                <button @click="saveTasks()" class="btn btn-primary" type="submit">Submit</button>
+        <div class="container d-flex justify-content-center">
+            <div class="col-4">
+                <ul class="list-group">
+                    <li class="list-group-item" v-for="task in todoList">{{task.text}}</li>
+                </ul>
+                <div>
+                    <input @keyup.enter="saveTasks()" v-model='task' class="form-control my-2" type="text" name="new_task" id="new_task" placeholder="Insert new task">
+                    <button @click="saveTasks()" class="btn btn-primary" type="submit">Submit</button>
+                </div>
             </div>
         </div>
     </div>
-
 </body>
 
 
